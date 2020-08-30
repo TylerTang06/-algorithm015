21512489a691cdb3dc47acaf670c3dd82b48dc50
tylertang <tylertang06@163.com>
2020-08-31 02:02:09 +0800

    add 42-trap code and update readme

---
+++ Week_01/42-trap.go
@@ -0,0 +1,29 @@
+package week01
+
+func trap(height []int) int {
+	if height == nil || len(height) <= 2 {
+		return 0
+	}
+	maxL, maxR, sum := 0, 0, 0
+	left, right := 1, len(height)-2
+	for i := 1; i < len(height)-1; i++ {
+		if height[left-1] < height[right+1] {
+			if maxL < height[left-1] {
+				maxL = height[left-1]
+			}
+			if maxL > height[left] {
+				sum += (maxL - height[left])
+			}
+			left++
+		} else {
+			if maxR < height[right+1] {
+				maxR = height[right+1]
+			}
+			if maxR > height[right] {
+				sum += (maxR - height[right])
+			}
+			right--
+		}
+	}
+	return sum
+}
